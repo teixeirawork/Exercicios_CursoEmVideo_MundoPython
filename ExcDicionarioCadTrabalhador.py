0b27030da9e6fba2e24851bd2f7b2f4b8645af30
Victor Teixeira Lima Costa <vteixeirawork@gmail.com>
2025-09-23 19:50:17 -0300

    adicionando resp guanabara
    apenas documentando as técnicas utilizada pelo professor do curso

--- ExcDicionarioCadTrabalhador.py
+++ ExcDicionarioCadTrabalhador.py
@@ -63,3 +63,25 @@ for idx, t in enumerate(trabalhador,1): # neste for define-se  t como um novo di
     for chave, valor in t.items():  # este for percorre o novo dicionaro t
         print(f' {chave}: {valor}')
     print('----')
+    
+    
+    """
+    codigo criado pelo guanabara para resolução do exercicio
+    
+    from datetime import datetime
+    dados = dict()
+    dados['nome'] = str(input('Nome: '))
+    nasc = int(input('Ano de Nascimento: '))
+    dados['idade'] = datetime.now().year - nasc
+    dados['cpts'] = int(input('Carteira de Trabalho 0 não tem: '))
+    if dados['cpts'] != 0:
+        dados['contratacao'] = int(input('Ano de contratação: '))
+        dados['salario'] = float(input('Salario R$: '))
+        dados ['Aposentadoria'] = dados['idade'] + ((dados[contratacao] +35)- datetime.now().year)
+    print('-='*30)
+    for k , v in dados.items():
+        print(f'{k} tem o valor {v}')
+    
+    
+    
+    """
