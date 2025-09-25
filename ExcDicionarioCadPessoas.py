4997250d1b93b55a99280df38accee72bde41ddf
Victor Teixeira Lima Costa <vteixeirawork@gmail.com>
2025-09-25 15:49:26 -0300

    exercicio 94 resolvido
    upando exercício resolvido antes de assistir a aula

---
+++ ExcDicionarioCadPessoas.py
@@ -0,0 +1,68 @@
+##Exercício Python 094: 
+# Crie um programa que leia nome, sexo e idade de várias pessoas, 
+# guardando os dados de cada pessoa em um dicionário e todos os dicionários em uma lista. 
+# No final, mostre: 
+# A) Quantas pessoas foram cadastradas 
+# B) A média de idade 
+# C) Uma lista com as mulheres 
+# D) Uma lista de pessoas com idade acima da média
+
+from datetime import datetime
+mulher = list()
+dados = list()
+
+media = 0
+
+while True:
+    
+    pessoa = dict()
+    
+    pessoa['nome'] = str(input('Digite seu nome: '))
+    pessoa['sexo'] = str(input('Digite o sexo [F/M]: ')).upper().strip()[0]
+    nasc = int(input('Digite o ano de seu nascimento: '))
+    ano = datetime.now().year
+    pessoa['idade'] = ano - nasc
+
+    
+    dados.append(pessoa.copy()) 
+    
+    media += pessoa['idade']
+    
+    if pessoa['sexo'] == 'F':
+        mulher.append(pessoa.copy())
+    
+
+    
+    opcao = str(input('Deseja Continuar [S/N]: ')).upper().strip()[0]
+    
+    while opcao not in 'SN':
+        opcao = str(input('Opcao inválida! Deseja continuar [S/N]')).upper().strip()[0]
+        
+    if opcao == 'N':
+        break
+    
+print('-='*30)    
+
+print(f'\n A - Total de pessoas cadastradas: {len(dados)}')
+
+print('-='*30)
+
+print(f'B- A média das idades: {media/len(dados):.2f}')
+
+acimamedia = []
+for p in dados:
+    if p['idade'] > (media/len(dados)):
+        acimamedia.append(p.copy())
+
+print('-='*30)
+
+print(f'C- Quem esta acima da media: {acimamedia}')
+
+print('-='*30)
+print(f'{dados}')
+
+print('-='*30)
+print(f'\n D - As mulheres são: {mulher}')
+
+
+print('-----------------FIM----------------')
